1a63d4f87e34ecafd1bf5934958abaef7984ce1f
electronicskitten <ethernetcabe@gmail.com>
2018-01-03 18:48:14 -0200

    done!

---
+++ A10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (int args, char *argv[])
+{
+int j;
+for(j=0; j < 25; j++)
+{
+printf("%d", j);
+if  (j && !(j%5))
+printf("\n");
+}
+getchar();
+return 0;
+}
---
+++ A102.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int args, char *argv[])
+{
+int j;
+for(j=0;j < 25; j +=5)
+{
+int i;
+for (i = j; i < (j+5); i++)
+printf("%d",i);
+printf("\n");
+}
+getchar();
+return 0;
+}
