b903572123558f3d89da97c8d12c48b4c4b7ff4f
electronicskitten <ethernetcabe@gmail.com>
2018-01-10 21:44:07 -0200

    identado

--- A20.c
+++ A20.c
@@ -3,21 +3,22 @@
 
 int main(int args, char *argv[])
 {
-int row, j, spaces, stars;
-int n = 12;
-for(row = 0; row < n; row++)
-{
-stars = 2*row + 1;
-spaces = (2*n - stars) / 2;
-for(j = 0; j < spaces; j++)
-printf(".");
-for(j = 0; j < stars; j++)
-printf("*");
-for(j = 0; j < spaces; j++)
-printf(".");
-printf("\n");
-}
-printf("\n");
-getchar();
-return 0;
+  int row, j, spaces, stars;
+  int n = 12;
+  s
+  for (row = 0; row < n; row++)
+  {
+    stars = 2 * row + 1;
+    spaces = (2 * n - stars) / 2;
+    for (j = 0; j < spaces; j++)
+      printf(".");
+    for (j = 0; j < stars; j++)
+      printf("*");
+    for (j = 0; j < spaces; j++)
+      printf(".");
+    printf("\n");
+  }
+  printf("\n");
+  getchar();
+  return 0;
 }
